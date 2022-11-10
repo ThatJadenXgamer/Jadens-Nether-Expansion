af8f07780ee2028ce4cf9075b412d1df46026d22
ThatJadenXgamer <jadenxgamer26@gmail.com>
2022-11-10 22:04:10 +0530

    Initial commit

---
+++ src/main/java/net/jadenxgamer/netherexp/NetherExp.java
@@ -0,0 +1,15 @@
+package net.jadenxgamer.netherexp;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class NetherExp implements ModInitializer {
+	public static final String MOD_ID = "netherexp";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/jadenxgamer/netherexp/NetherExpClient.java
@@ -0,0 +1,10 @@
+package net.jadenxgamer.netherexp;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class NetherExpClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
