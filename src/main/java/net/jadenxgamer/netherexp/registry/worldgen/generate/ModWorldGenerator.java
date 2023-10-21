ed9d4de496df3d4b27a159be408ba4cde86dffe6
ThatJadenXgamer <chandig83@gmail.com>
2023-10-21 20:52:14 +0530

    -Added  -Crimson Sporeshroom now naturally generates  -Weeping Ivy now also naturally generates  -Quartz Crystals now naturally generate in Nether Wastes   -More Likely to appear on top of Nether Quartz Ore
    -Changes
     -Warphoppers are now Immune to Fire
     -Drastically Increased Wart Beard Generation
     -Warped Wart Spots are now more common as well
     -Bone Rods no longer spread out as much when generating
     -Piglins are no longer repelled by Ecto Soul Sand or Soul Magma Blocks
     -Piglins can however be repelled by Soul Jack O'Lanterns and Ghoul O'Lanterns
     -Jagged Soul Slate now generates in disk formations rather than blobs
     -Bone Rods produce Step Sounds when inside the block

--- src/main/java/net/jadenxgamer/netherexp/registry/worldgen/generate/ModWorldGenerator.java
+++ src/main/java/net/jadenxgamer/netherexp/registry/worldgen/generate/ModWorldGenerator.java
@@ -2,6 +2,7 @@ package net.jadenxgamer.netherexp.registry.worldgen.generate;
 
 public class ModWorldGenerator {
     public static void generateModWorldGen() {
+        NetherWastesFeatures.generateFeatures();
         SoulSandValleyFeatures.generateFeatures();
         CrimsonForestFeatures.generateFeatures();
         WarpedForestFeatures.generateFeatures();
