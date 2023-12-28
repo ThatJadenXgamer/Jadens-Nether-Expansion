fa13d50167dd65d9d0af4562afec6034a3fbd539
ThatJadenXgamer <chandig83@gmail.com>
2023-12-28 20:09:01 +0530

    Temporarily Removed Vent Mire Generation
    Changes
    -Vent Mire no longer naturally generates
    -Wrath Region & Wrath Configs were removed as well

--- src/main/java/net/jadenxgamer/netherexp/registry/worldgen/biome/NetherExpTerraBlender.java
+++ src/main/java/net/jadenxgamer/netherexp/registry/worldgen/biome/NetherExpTerraBlender.java
@@ -1,16 +1,11 @@
 package net.jadenxgamer.netherexp.registry.worldgen.biome;
 
-import net.jadenxgamer.netherexp.NetherExp;
-import net.jadenxgamer.netherexp.registry.worldgen.biome.nether.WrathRegion;
-import net.minecraft.util.Identifier;
-import terrablender.api.Regions;
 import terrablender.api.TerraBlenderApi;
 
 public class NetherExpTerraBlender implements TerraBlenderApi {
 
     @Override
     public void onTerraBlenderInitialized() {
-        // Generates Vent Mire
-        Regions.register(new WrathRegion(new Identifier(NetherExp.MOD_ID, "wrath"), NetherExp.getConfig().worldgen.wrath_region_weight));
+
     }
 }
