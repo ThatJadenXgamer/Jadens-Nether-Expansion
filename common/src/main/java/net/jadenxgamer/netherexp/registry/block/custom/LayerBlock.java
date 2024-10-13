2fa30a3ca95c96ab4451513d614b56d907dada1e
Wawwior <45405580+Wawwior@users.noreply.github.com>
2024-10-13 23:52:56 +0200

    Feature: Snow! Real magic! Compatibility
    Fixes #23
    Not happy to basically just Copy-Paste the class but oh well

---
+++ common/src/main/java/net/jadenxgamer/netherexp/registry/block/custom/LayerBlock.java
@@ -0,0 +1,120 @@
+
+package net.jadenxgamer.netherexp.registry.block.custom;
+
+import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.LevelAccessor;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition.Builder;
+import net.minecraft.world.level.block.state.properties.BlockStateProperties;
+import net.minecraft.world.level.block.state.properties.IntegerProperty;
+import net.minecraft.world.level.block.state.properties.Property;
+import net.minecraft.world.level.pathfinder.PathComputationType;
+import net.minecraft.world.phys.shapes.CollisionContext;
+import net.minecraft.world.phys.shapes.Shapes;
+import net.minecraft.world.phys.shapes.VoxelShape;
+
+/**
+ * LayerBlock
+ */
+public class LayerBlock extends Block {
+
+    public static final int MAX_HEIGHT = 8;
+    private final int height_impassable;
+    public static final IntegerProperty LAYERS = BlockStateProperties.LAYERS; 
+    public static final VoxelShape[] SHAPE_BY_LAYER = new VoxelShape[]{
+        Shapes.empty(), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 2.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 4.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 6.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 8.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 10.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 12.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 14.0, 16.0), 
+        Block.box(0.0, 0.0, 0.0, 16.0, 16.0, 16.0)
+    };
+
+    public LayerBlock(Properties properties, int height_impassable) {
+        super(properties);
+        this.height_impassable = height_impassable;
+        this.registerDefaultState(this.stateDefinition.any().setValue(LAYERS, 1));
+    }
+
+    @Override
+    public boolean isPathfindable(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, PathComputationType pathComputationType) {
+        switch (pathComputationType) {
+            case LAND:
+                return blockState.getValue(LAYERS) < height_impassable; 
+            default:
+                return false;
+        }
+    }
+
+    @Override
+    public VoxelShape getShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    @Override
+    public VoxelShape getCollisionShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    @Override
+    public VoxelShape getVisualShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos, CollisionContext collisionContext) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+    
+    @Override
+    public VoxelShape getBlockSupportShape(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return SHAPE_BY_LAYER[blockState.getValue(LAYERS)];
+    }
+
+    @Override
+    public boolean useShapeForLightOcclusion(BlockState blockState) {
+        return true;
+    }
+
+    @Override
+    public float getShadeBrightness(BlockState blockState, BlockGetter blockGetter, BlockPos blockPos) {
+        return blockState.getValue(LAYERS) == 8 ? 0.2F : 1.0F;
+    }
+   
+    @Override
+    public BlockState updateShape(BlockState blockState, Direction direction, BlockState blockState2,
+            LevelAccessor levelAccessor, BlockPos blockPos, BlockPos blockPos2) {
+        return blockState.canSurvive(levelAccessor, blockPos) ? Blocks.AIR.defaultBlockState() : blockState.updateShape(direction, blockState, levelAccessor, blockPos, blockPos2);
+    }
+    
+    @Override
+    public boolean canBeReplaced(BlockState blockState, BlockPlaceContext blockPlaceContext) {
+        int layers = blockState.getValue(LAYERS);
+        if (blockPlaceContext.getItemInHand().is(this.asItem()) && layers < 8) {
+            if (blockPlaceContext.replacingClickedOnBlock()) {
+                return blockPlaceContext.getClickedFace() == Direction.UP;
+            }
+            return true;
+        } 
+        return layers == 1;
+    }
+    
+    @Override
+    public BlockState getStateForPlacement(BlockPlaceContext blockPlaceContext) {
+        BlockState state = blockPlaceContext.getLevel().getBlockState(blockPlaceContext.getClickedPos());
+        if (state.is(this)) {
+            int layers = state.getValue(LAYERS);
+            return state.setValue(LAYERS, Math.min(8, layers + 1));
+        }
+        return super.getStateForPlacement(blockPlaceContext);
+    }
+
+    @Override
+    protected void createBlockStateDefinition(Builder<Block, BlockState> builder) {
+        builder.add(new Property[]{LAYERS});
+    }
+
+}
