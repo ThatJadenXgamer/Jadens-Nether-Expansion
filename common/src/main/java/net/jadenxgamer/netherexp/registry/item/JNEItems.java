485b30e2c067bc6cf0941b4730f8e0a10b4b27fe
Wawwior <45405580+Wawwior@users.noreply.github.com>
2024-10-27 19:08:13 +0100

    Fix: Wisp Bottle Interaction
    Fixes #62

--- common/src/main/java/net/jadenxgamer/netherexp/registry/item/JNEItems.java
+++ common/src/main/java/net/jadenxgamer/netherexp/registry/item/JNEItems.java
@@ -1,5 +1,7 @@
 package net.jadenxgamer.netherexp.registry.item;
 
+import java.util.function.Supplier;
+
 import dev.architectury.core.item.ArchitecturySpawnEggItem;
 import dev.architectury.registry.fuel.FuelRegistry;
 import dev.architectury.registry.registries.DeferredRegister;
@@ -7,197 +9,215 @@ import dev.architectury.registry.registries.RegistrySupplier;
 import net.jadenxgamer.netherexp.NetherExp;
 import net.jadenxgamer.netherexp.registry.block.JNEBlocks;
 import net.jadenxgamer.netherexp.registry.entity.JNEEntityType;
-import net.jadenxgamer.netherexp.registry.item.custom.*;
+import net.jadenxgamer.netherexp.registry.item.custom.AntidoteItem;
+import net.jadenxgamer.netherexp.registry.item.custom.GrenadeAntidoteItem;
+import net.jadenxgamer.netherexp.registry.item.custom.LightsporesItem;
+import net.jadenxgamer.netherexp.registry.item.custom.MistChargeItem;
+import net.jadenxgamer.netherexp.registry.item.custom.MobBottleItem;
+import net.jadenxgamer.netherexp.registry.item.custom.NetheriteArtifactItem;
+import net.jadenxgamer.netherexp.registry.item.custom.NightsporesItem;
+import net.jadenxgamer.netherexp.registry.item.custom.PhasmoArrowItem;
+import net.jadenxgamer.netherexp.registry.item.custom.PumpChargeShotgunItem;
+import net.jadenxgamer.netherexp.registry.item.custom.PumpChargeSmithingTemplateItem;
+import net.jadenxgamer.netherexp.registry.item.custom.SanctumCompassItem;
+import net.jadenxgamer.netherexp.registry.item.custom.ShotgunFistItem;
+import net.jadenxgamer.netherexp.registry.item.custom.TreacherousFlameItem;
+import net.jadenxgamer.netherexp.registry.item.custom.WillOWispItem;
 import net.jadenxgamer.netherexp.registry.misc_registry.JNESoundEvents;
 import net.jadenxgamer.netherexp.registry.misc_registry.JNETrimPatterns;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.sounds.SoundEvents;
-import net.minecraft.world.item.*;
-
-import java.util.function.Supplier;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.HangingSignItem;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.Rarity;
+import net.minecraft.world.item.RecordItem;
+import net.minecraft.world.item.SignItem;
+import net.minecraft.world.item.SmithingTemplateItem;
 
 @SuppressWarnings("unused")
 public class JNEItems {
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(NetherExp.MOD_ID, Registries.ITEM);
 
     public static final RegistrySupplier<Item> WARPHOPPER_FUR = registerItem("warphopper_fur", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> HOGHAM = registerItem("hogham", () ->
-            new Item(new Item.Properties().food(JNEFoods.HOGHAM)));
+        new Item(new Item.Properties().food(JNEFoods.HOGHAM)));
 
     public static final RegistrySupplier<Item> COOKED_HOGHAM = registerItem("cooked_hogham", () ->
-            new Item(new Item.Properties().food(JNEFoods.COOKED_HOGHAM)));
+        new Item(new Item.Properties().food(JNEFoods.COOKED_HOGHAM)));
 
     public static final RegistrySupplier<Item> FOGGY_ESSENCE = registerItem("foggy_essence", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> IRON_SCRAP = registerItem("iron_scrap", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> WHITE_ASH_POWDER = registerItem("white_ash_powder", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> FOSSIL_FUEL = registerFuel("fossil_fuel", () ->
-            new Item(new Item.Properties()), 100);
+        new Item(new Item.Properties()), 100);
 
     public static final RegistrySupplier<Item> RIFT_ARMOR_TRIM_SMITHING_TEMPLATE = registerItem("rift_armor_trim_smithing_template", () ->
-            SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.RIFT));
+        SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.RIFT));
 
     public static final RegistrySupplier<Item> SPIRIT_ARMOR_TRIM_SMITHING_TEMPLATE = registerItem("spirit_armor_trim_smithing_template", () ->
-            SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.SPIRIT));
+        SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.SPIRIT));
 
     public static final RegistrySupplier<Item> VALOR_ARMOR_TRIM_SMITHING_TEMPLATE = registerItem("valor_armor_trim_smithing_template", () ->
-            SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.VALOR));
+        SmithingTemplateItem.createArmorTrimTemplate(JNETrimPatterns.VALOR));
 
     public static final RegistrySupplier<Item> PUMP_CHARGE_UPGRADE_SMITHING_TEMPLATE = registerItem("pump_charge_upgrade_smithing_template",
-            PumpChargeSmithingTemplateItem::new);
+        PumpChargeSmithingTemplateItem::new);
 
     public static final RegistrySupplier<Item> LIGHTSPORES = registerItem("lightspores", () ->
-            new LightsporesItem(new Item.Properties()));
+        new LightsporesItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> NIGHTSPORES = registerItem("nightspores", () ->
-            new NightsporesItem(new Item.Properties()));
+        new NightsporesItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> BLIGHTSPORES = registerItem("blightspores", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> FRIGHTSPORES = registerItem("frightspores", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> ANTIDOTE = registerItem("antidote", () ->
-            new AntidoteItem(new Item.Properties()));
+        new AntidoteItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> GRENADE_ANTIDOTE = registerItem("grenade_antidote", () ->
-            new GrenadeAntidoteItem(new Item.Properties()));
+        new GrenadeAntidoteItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> GLOWCHEESE = registerItem("glowcheese", () ->
-            new Item(new Item.Properties().food(JNEFoods.GLOWCHEESE)));
+        new Item(new Item.Properties().food(JNEFoods.GLOWCHEESE)));
 
     public static final RegistrySupplier<Item> NETHER_PIZZA_SLICE = registerItem("nether_pizza_slice", () ->
-            new Item(new Item.Properties().food(JNEFoods.PIZZA_SLICE).rarity(Rarity.RARE)));
+        new Item(new Item.Properties().food(JNEFoods.PIZZA_SLICE).rarity(Rarity.RARE)));
 
     public static final RegistrySupplier<Item> WISP_BOTTLE = registerItem("wisp_bottle", () ->
-            new MobBottleItem(SoundEvents.BOTTLE_EMPTY, new Item.Properties().stacksTo(1).craftRemainder(Items.GLASS_BOTTLE)));
+        new MobBottleItem(() -> JNEEntityType.WISP.get(), () -> SoundEvents.BOTTLE_EMPTY, new Item.Properties().stacksTo(1).craftRemainder(Items.GLASS_BOTTLE)));
 
     public static final RegistrySupplier<Item> SANCTUM_COMPASS = registerItem("sanctum_compass", () ->
-            new SanctumCompassItem(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
+        new SanctumCompassItem(new Item.Properties().stacksTo(1).rarity(Rarity.RARE)));
 
     public static final RegistrySupplier<Item> WRAITHING_FLESH = registerItem("wraithing_flesh", () ->
-            new Item(new Item.Properties().food(JNEFoods.WRAITHING_FLESH)));
+        new Item(new Item.Properties().food(JNEFoods.WRAITHING_FLESH)));
 
     public static final RegistrySupplier<Item> PHASMO_SHARD = registerItem("phasmo_shard", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> PHASMO_ARROW = registerItem("phasmo_arrow", () ->
-            new PhasmoArrowItem(new Item.Properties()));
+        new PhasmoArrowItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> WARPED_WART = registerItem("warped_wart", () ->
-            new BlockItem(JNEBlocks.WARPED_WART.get(), new Item.Properties()));
+        new BlockItem(JNEBlocks.WARPED_WART.get(), new Item.Properties()));
 
     public static final RegistrySupplier<Item> MIST_CHARGE = registerItem("mist_charge", () ->
-            new MistChargeItem(new Item.Properties()));
+        new MistChargeItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> BANSHEE_ROD = registerItem("banshee_rod", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> BANSHEE_POWDER = registerItem("banshee_powder", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> WILL_O_WISP = registerItem("will_o_wisp", () ->
-            new WillOWispItem(new Item.Properties()));
+        new WillOWispItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> STRIDITE = registerItem("stridite", () ->
-            new Item(new Item.Properties().fireResistant()));
+        new Item(new Item.Properties().fireResistant()));
 
     public static final RegistrySupplier<Item> NETHERITE_PLATING = registerItem("netherite_plating", () ->
-            new Item(new Item.Properties().fireResistant()));
+        new Item(new Item.Properties().fireResistant()));
 
     public static final RegistrySupplier<Item> TREACHEROUS_FLAME = registerItem("treacherous_flame", () ->
-            new TreacherousFlameItem(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
+        new TreacherousFlameItem(new Item.Properties().rarity(Rarity.RARE).fireResistant()));
 
     public static final RegistrySupplier<Item> SHOTGUN_FIST = registerItem("shotgun_fist", () ->
-            new ShotgunFistItem(new Item.Properties().stacksTo(1).durability(512).fireResistant().rarity(Rarity.RARE)));
+        new ShotgunFistItem(new Item.Properties().stacksTo(1).durability(512).fireResistant().rarity(Rarity.RARE)));
 
     public static final RegistrySupplier<Item> PUMP_CHARGE_SHOTGUN = registerItem("pump_charge_shotgun", () ->
-            new PumpChargeShotgunItem(new Item.Properties().stacksTo(1).durability(640).fireResistant().rarity(Rarity.EPIC)));
+        new PumpChargeShotgunItem(new Item.Properties().stacksTo(1).durability(640).fireResistant().rarity(Rarity.EPIC)));
 
     public static final RegistrySupplier<Item> CLARET_SIGN = registerItem("claret_sign", () ->
-            new SignItem(new Item.Properties().stacksTo(16), JNEBlocks.CLARET_SIGN.get(), JNEBlocks.CLARET_WALL_SIGN.get()));
+        new SignItem(new Item.Properties().stacksTo(16), JNEBlocks.CLARET_SIGN.get(), JNEBlocks.CLARET_WALL_SIGN.get()));
 
     public static final RegistrySupplier<Item> CLARET_HANGING_SIGN = registerItem("claret_hanging_sign", () ->
-            new HangingSignItem(JNEBlocks.CLARET_HANGING_SIGN.get(), JNEBlocks.CLARET_WALL_HANGING_SIGN.get(), new Item.Properties().stacksTo(16)));
+        new HangingSignItem(JNEBlocks.CLARET_HANGING_SIGN.get(), JNEBlocks.CLARET_WALL_HANGING_SIGN.get(), new Item.Properties().stacksTo(16)));
 
     public static final RegistrySupplier<Item> SMOKESTALK_SIGN = registerItem("smokestalk_sign", () ->
-            new SignItem(new Item.Properties().stacksTo(16), JNEBlocks.SMOKESTALK_SIGN.get(), JNEBlocks.SMOKESTALK_WALL_SIGN.get()));
+        new SignItem(new Item.Properties().stacksTo(16), JNEBlocks.SMOKESTALK_SIGN.get(), JNEBlocks.SMOKESTALK_WALL_SIGN.get()));
 
     public static final RegistrySupplier<Item> SMOKESTALK_HANGING_SIGN = registerItem("smokestalk_hanging_sign", () ->
-            new HangingSignItem(JNEBlocks.SMOKESTALK_HANGING_SIGN.get(), JNEBlocks.SMOKESTALK_WALL_HANGING_SIGN.get(), new Item.Properties().stacksTo(16)));
+        new HangingSignItem(JNEBlocks.SMOKESTALK_HANGING_SIGN.get(), JNEBlocks.SMOKESTALK_WALL_HANGING_SIGN.get(), new Item.Properties().stacksTo(16)));
 
     public static final RegistrySupplier<Item> MUSIC_DISC_CRICKET  = registerItem("music_disc_cricket", () ->
-            new RecordItem(13, JNESoundEvents.MUSIC_DISC_CRICKET.get(), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 164));
+        new RecordItem(13, JNESoundEvents.MUSIC_DISC_CRICKET.get(), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 164));
 
     public static final RegistrySupplier<Item> MUSIC_DISC_BUCKSHOT_WONDERLAND = registerItem("music_disc_buckshot_wonderland", () ->
-            new RecordItem(8, JNESoundEvents.MUSIC_DISC_BUCKSHOT_WONDERLAND.get(), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 193));
+        new RecordItem(8, JNESoundEvents.MUSIC_DISC_BUCKSHOT_WONDERLAND.get(), new Item.Properties().stacksTo(1).rarity(Rarity.RARE), 193));
 
     public static final RegistrySupplier<Item> SKULL_ON_A_STICK = registerItem("skull_on_a_stick", () ->
-            new JNEFoodOnAStickItem(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).durability(100)));
+        new JNEFoodOnAStickItem(new Item.Properties().stacksTo(1).rarity(Rarity.UNCOMMON).durability(100)));
 
     public static final RegistrySupplier<Item> APPARITION_SPAWN_EGG = registerItem("apparition_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.APPARITION, 4864303, 699311, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.APPARITION, 4864303, 699311, new Item.Properties()));
 
     public static final RegistrySupplier<Item> WISP_SPAWN_EGG = registerItem("wisp_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.WISP, 6022120, 699311, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.WISP, 6022120, 699311, new Item.Properties()));
 
     public static final RegistrySupplier<Item> VESSEL_SPAWN_EGG = registerItem("vessel_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.VESSEL, 4864303, 12698049, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.VESSEL, 4864303, 12698049, new Item.Properties()));
 
     public static final RegistrySupplier<Item> ECTO_SLAB_SPAWN_EGG = registerItem("ecto_slab_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.ECTO_SLAB, 4864303, 1788232, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.ECTO_SLAB, 4864303, 1788232, new Item.Properties()));
 
     public static final RegistrySupplier<Item> BANSHEE_SPAWN_EGG = registerItem("banshee_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.BANSHEE, 1985382, 1788232, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.BANSHEE, 1985382, 1788232, new Item.Properties()));
 
     public static final RegistrySupplier<Item> STAMPEDE_SPAWN_EGG = registerItem("stampede_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.STAMPEDE, 4864303, 10236982, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.STAMPEDE, 4864303, 10236982, new Item.Properties()));
 
     public static final RegistrySupplier<Item> CARCASS_SPAWN_EGG = registerItem("carcass_spawn_egg", () ->
-            new ArchitecturySpawnEggItem(JNEEntityType.CARCASS, 8263192, 4066060, new Item.Properties()));
+        new ArchitecturySpawnEggItem(JNEEntityType.CARCASS, 8263192, 4066060, new Item.Properties()));
 
     public static final RegistrySupplier<Item> SEALED_POTTERY_SHERD = registerItem("sealed_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> SPECTRE_POTTERY_SHERD = registerItem("spectre_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> MARIONETTE_POTTERY_SHERD = registerItem("marionette_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> ELDRITCH_POTTERY_SHERD = registerItem("eldritch_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> DECEPTION_POTTERY_SHERD = registerItem("deception_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> FIREARM_POTTERY_SHERD = registerItem("firearm_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> BOTANICAL_POTTERY_SHERD = registerItem("botanical_pottery_sherd", () ->
-            new Item(new Item.Properties()));
+        new Item(new Item.Properties()));
 
     public static final RegistrySupplier<Item> CEREBRAGE = registerItem("cerebrage", () ->
-            new Item(new Item.Properties().food(JNEFoods.CEREBRAGE)));
+        new Item(new Item.Properties().food(JNEFoods.CEREBRAGE)));
 
     public static final RegistrySupplier<Item> CEREBRAGE_SEEDS = registerItem("cerebrage_seeds", () ->
-            new CerebrageSeedItem(new Item.Properties()));
+        new CerebrageSeedItem(new Item.Properties()));
 
     public static final RegistrySupplier<Item> ROASTED_BONE = registerItem("roasted_bone", () ->
-            new Item(new Item.Properties().food(JNEFoods.ROASTED_BONE).stacksTo(16)));
+        new Item(new Item.Properties().food(JNEFoods.ROASTED_BONE).stacksTo(16)));
 
     // NETHERITE ARTIFACTS
 
     public static final RegistrySupplier<Item> SHOTGUN_CORE = registerItem("shotgun_core", () ->
-            new NetheriteArtifactItem(new Item.Properties().fireResistant().stacksTo(1).rarity(Rarity.RARE)));
+        new NetheriteArtifactItem(new Item.Properties().fireResistant().stacksTo(1).rarity(Rarity.RARE)));
 
     private static <T extends Item> RegistrySupplier<T> registerItem(String name, Supplier<T> item) {
         return ITEMS.register(name, item);
--- common/src/main/java/net/jadenxgamer/netherexp/registry/item/custom/MobBottleItem.java
+++ common/src/main/java/net/jadenxgamer/netherexp/registry/item/custom/MobBottleItem.java
@@ -1,62 +1,90 @@
 package net.jadenxgamer.netherexp.registry.item.custom;
 
-import net.jadenxgamer.netherexp.registry.entity.JNEEntityType;
+import com.google.common.base.Supplier;
+
 import net.jadenxgamer.netherexp.registry.entity.custom.Bottleable;
 import net.minecraft.core.BlockPos;
+import net.minecraft.core.Direction;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.sounds.SoundEvent;
+import net.minecraft.sounds.SoundEvents;
 import net.minecraft.sounds.SoundSource;
-import net.minecraft.world.InteractionResult;
+import net.minecraft.stats.Stats;
+import net.minecraft.world.InteractionHand;
+import net.minecraft.world.InteractionResultHolder;
 import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobSpawnType;
-import net.minecraft.world.entity.animal.Bucketable;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.ItemStack;
-import net.minecraft.world.item.ItemUtils;
 import net.minecraft.world.item.Items;
-import net.minecraft.world.item.context.UseOnContext;
+import net.minecraft.world.level.ClipContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.gameevent.GameEvent;
-import org.jetbrains.annotations.NotNull;
+import net.minecraft.world.phys.BlockHitResult;
+import net.minecraft.world.phys.HitResult.Type;
 
+/**
+ * MobBottleItem
+ */
 public class MobBottleItem extends Item {
-    private final SoundEvent emptySound;
 
-    public MobBottleItem(SoundEvent emptySound, Properties properties) {
+    private Supplier<EntityType<?>> entityTypeSupplier;
+    private Supplier<SoundEvent> soundEventSupplier;
+
+    public MobBottleItem(Supplier<EntityType<?>> entityTypeSupplier, Properties properties) {
+        this(entityTypeSupplier, () -> {
+            return SoundEvents.BOTTLE_EMPTY;
+        }, properties);
+    }
+
+    public MobBottleItem(Supplier<EntityType<?>> entityTypeSupplier, Supplier<SoundEvent> soundEventSupplier, Properties properties) {
         super(properties);
-        this.emptySound = emptySound;
+        this.entityTypeSupplier = entityTypeSupplier;
+        this.soundEventSupplier = soundEventSupplier;
+    }
+
+    public EntityType<?> getEntityType() {
+        return entityTypeSupplier.get();
+    }
+
+    public SoundEvent getSoundEvent() {
+        return soundEventSupplier.get();
     }
 
     @Override
-    public @NotNull InteractionResult useOn(UseOnContext context) {
-        Player player = context.getPlayer();
-        Level level = context.getLevel();
-        ItemStack stack = context.getItemInHand();
-        BlockPos pos = context.getClickedPos();
-        if (level instanceof ServerLevel) {
-            this.spawn((ServerLevel) level, stack, pos);
-            this.playEmptySound(level, pos);
-            level.gameEvent(player, GameEvent.ENTITY_PLACE, pos);
-            assert player != null;
-            if (!player.isCreative()) {
-                player.setItemInHand(context.getHand(), ItemUtils.createFilledResult(stack, player, new ItemStack(Items.GLASS_BOTTLE)));
+    public InteractionResultHolder<ItemStack> use(Level level, Player player, InteractionHand interactionHand) {
+        ItemStack itemStack = player.getItemInHand(interactionHand);
+        BlockHitResult blockHitResult = getPlayerPOVHitResult(level, player, ClipContext.Fluid.NONE);
+        if (blockHitResult.getType() == Type.MISS) {
+            return InteractionResultHolder.pass(itemStack);
+        } else if (blockHitResult.getType() != Type.BLOCK) {
+            return InteractionResultHolder.pass(itemStack);
+        } else {
+            BlockPos pos = blockHitResult.getBlockPos();
+            Direction direction = blockHitResult.getDirection();
+            BlockPos relativePos = pos.relative(direction);
+            if (level.mayInteract(player, pos) && player.mayUseItemAt(relativePos, direction, itemStack)) {
+                level.playSound(player, relativePos, this.getSoundEvent(), SoundSource.NEUTRAL, 1.0f, 1.0f);
+                if (level instanceof ServerLevel) {
+                    Entity entity = this.getEntityType().spawn((ServerLevel) level, itemStack, null, relativePos, MobSpawnType.BUCKET, true, false);
+                    if (entity instanceof Bottleable bottleable) {
+                        bottleable.loadFromBottleTag(itemStack.getOrCreateTag());
+                        bottleable.setFromBottle(true);
+                    }
+                    level.gameEvent(player, GameEvent.ENTITY_PLACE, relativePos);
+                }
+
+                player.awardStat(Stats.ITEM_USED.get(this));
+                return InteractionResultHolder.sidedSuccess(getEmptySuccessItem(itemStack, player), level.isClientSide());
+            } else {
+                return InteractionResultHolder.fail(itemStack);
             }
-            return InteractionResult.sidedSuccess(level.isClientSide);
         }
-        return super.useOn(context);
     }
 
-    protected void playEmptySound(LevelAccessor levelAccessor, BlockPos blockPos) {
-        levelAccessor.playSound(null, blockPos, this.emptySound, SoundSource.NEUTRAL, 1.0F, 1.0F);
-    }
-
-    private void spawn(ServerLevel serverLevel, ItemStack itemStack, BlockPos blockPos) {
-        Entity entity = JNEEntityType.WISP.get().spawn(serverLevel, itemStack, null, blockPos, MobSpawnType.BUCKET, true, false);
-        if (entity instanceof Bottleable bottleable) {
-            bottleable.loadFromBottleTag(itemStack.getOrCreateTag());
-            bottleable.setFromBottle(true);
-        }
+    public static ItemStack getEmptySuccessItem(ItemStack itemStack, Player player) {
+        return !player.getAbilities().instabuild ? new ItemStack(Items.GLASS_BOTTLE) : itemStack;
     }
 }
